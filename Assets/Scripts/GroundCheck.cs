e44aa5205c87a62a992ad95d99507bdf520c62fd
Alexander Jurcau <jurcaua@gmail.com>
2017-01-10 19:05:29 -0500

    basic movement and animations configured

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+	private PlayerMovement p;
+
+	// Use this for initialization
+	void Start () {
+		p = GetComponentInParent<PlayerMovement> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		p.setJumping (false);
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		p.setJumping (true);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	public float jumpHeight;
+
+	private Rigidbody2D r;
+	private SpriteRenderer s;
+	private Animator a;
+
+	private float horizontal;
+	private float vertical;
+
+	private bool jumping = false;
+	private float vel = 0f;
+
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<Rigidbody2D> ();
+		s = GetComponent<SpriteRenderer> ();
+		a = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		a.SetBool ("Jumping", jumping);
+		a.SetFloat ("Velocity", vel);
+	}
+
+	void FixedUpdate(){
+		horizontal = Input.GetAxisRaw ("Horizontal");
+		vertical = Input.GetAxisRaw ("Vertical");
+
+		vel = r.velocity.x;
+
+		r.AddForce(transform.right * horizontal * speed);
+		if(!jumping){
+			r.AddForce(transform.up * vertical * jumpHeight);
+		}
+	}
+
+	public void setJumping(bool toSet){
+		jumping = toSet;
+	}
+}
